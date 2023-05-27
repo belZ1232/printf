50514207c1cca250f331ead0288ad508518f27fa
Pkass313 <pkass313@gmail.com>
2023-05-27 10:47:51 -0700

    main.h

--- main.h
+++ main.h
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+/* Function prototypes */
+int _printf(const char *format, ...);
 
 #define BUFF_SIZE 1024
 #define UNUSED(x) (void)(x)
+
+#endif /* MAIN_H */
