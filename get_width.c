552c90961a73911ee851823b4b0cf2d79a80674b
belZ1232 <Belzsena@gmail.com>
2023-05-27 18:29:15 -0700

    Width

--- get_width.c
+++ get_width.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* get_width - calculations of width f field used in format string.
+* get_width - calculate width of field used in format string.
 * @format: our format string.
-* @i: current index in formart string.
-* @list: print va_list of arguments.
-* @Return: value of field width, or 0 if width is not found.
+* @i: current index in formart string
+* @list: print va_list of arguments
+* @Return: value of  width, or 0 if width is not found
 */
 int get_width(const char *format, int *i, va_list list)
 {
@@ -29,3 +29,4 @@ break;
 *i = curr_i - 1;
 return (width);
 }
+
